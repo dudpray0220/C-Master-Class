4a73932a88b0922fa676e5be274b16bc323985c3
YoungHyunB <qodudgus0220@naver.com>
2022-05-26 11:59:41 +0900

    CLR test

---
+++ 1-2.CLR/1-2.CLR/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _1_2.CLR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+            int c = a * b;
+            Console.WriteLine(c);
+            Console.ReadKey();
+        }
+    }
+}

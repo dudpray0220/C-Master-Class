e3e3b9d91af26f22d482603082a057576297733e
YoungHyunB <qodudgus0220@naver.com>
2022-05-27 11:48:19 +0900

    Section 2

---
+++ Section 2/ConsoleClassExample/ConsoleClassExample/Program.cs
@@ -0,0 +1,25 @@
+﻿class Sample
+{
+    static void Main()
+    {
+        // print line by line
+        System.Console.WriteLine("Welcome");
+        System.Console.WriteLine("To");
+        System.Console.WriteLine("C#");
+
+        // print side by side
+        System.Console.Write("Welcome ");
+        System.Console.Write("To ");
+        System.Console.Write("C#");
+
+        // 키 누를때 까지 대기
+        System.Console.ReadKey();
+
+        // clear the screen
+        System.Console.Clear();
+
+        // write
+        System.Console.WriteLine("God bless you");
+        System.Console.ReadKey();
+    }
+}
---
+++ Section 2/VariableExample/VariableExample/Program.cs
@@ -0,0 +1,19 @@
+﻿class Sample
+{
+    static void Main()
+    {
+        // 변수 선언
+        string stdentName = "Bae";
+        int age = 28;
+
+        // print value
+        System.Console.Write("Hey ");
+        System.Console.Write(stdentName);
+        System.Console.Write(" ,your age is ");
+        System.Console.Write(age);
+
+
+        // wait for press key
+        System.Console.ReadKey();
+    }
+}

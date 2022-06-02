53be8ea6939599ff0eee87a04a76cfa109157e16
YoungHyunB <qodudgus0220@naver.com>
2022-06-02 18:16:49 +0900

    modify

---
+++ Section 2/WhileExample/WhileExample/Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // 초기화
+        int i = 1;
+        // while Loop
+        while (i <= 10)
+        {
+            System.Console.Write(i + " ");
+            i++;
+        }
+        System.Console.WriteLine(); 
+
+        // 초기화
+        int a = 0;
+        // while Loop
+        while (a< 10)
+        {
+            System.Console.Write(a + " ");
+            a++;
+        }
+        System.Console.WriteLine();
+
+        // 초기화
+        int b = 9;
+        // while Loop
+        while (b >= 0)
+        {
+            System.Console.Write(b + " ");
+            b--;
+        }
+        System.Console.ReadKey();
+    }
+}

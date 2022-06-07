e8c1006ba840f0f8fc5e958cb6892d603ad58d1f
YoungHyunB <qodudgus0220@naver.com>
2022-06-07 13:43:13 +0900

    Section2

---
+++ Section 2/BreakExample/BreakExample/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    static void Main()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            if (i == 6)
+            {
+                break;
+            }
+            System.Console.Write(i + " ");
+        }
+        System.Console.ReadKey();
+    }
+}

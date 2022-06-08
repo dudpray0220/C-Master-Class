ecaf5a3b58d9df856cdbb6fac85fbd622c265c68
YoungHyunB <qodudgus0220@naver.com>
2022-06-08 11:57:41 +0900

    section 3 OOP

---
+++ Section 3/ClassExample/ClassExample/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // reference variables
+        Customer c1, c2;
+
+        // objects
+        c1 = new Customer();
+        c2 = new Customer();
+    }
+}

c16c25511b1a43cdfcfa0467bd98c68bc15ad2d0
YoungHyunB <qodudgus0220@naver.com>
2022-07-19 18:07:11 +0900

    modify

--- Section 3/ClassExample/ClassLibrary1/Class1.cs
+++ Section 3/ClassExample/ClassLibrary1/Class1.cs
@@ -1,11 +1,28 @@
 ﻿public class Customer
 {
-    public int customerID;
-    public string customerName;
+    private int customerID;
+    protected string customerName;
+    private protected double cost;
+}
+
+public class Child : Customer
+{
+    public void Method1()
+    {
+        customerID = 1;
+        customerName = "God bless you";
+        cost = 1000;
+    }
 }
 
 public class Employee
 {
     public int employeeID;
     public string employeeName;
+
+    public void Method1()
+    {
+        customerID = 1;
+        cost = 1000;
+    }
 }
